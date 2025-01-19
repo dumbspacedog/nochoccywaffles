d7e49041851b620298bbf9be1038b6069a440f43
dumbspacedog <hello@enbyandie.moe>
2025-01-19 15:10:38 -0500

    repair viewport export

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -22,9 +22,12 @@ export const metadata: Metadata = {
     title: "NoChoccyWaffles",
     description: "The catch-all portfolio for my projects. [If, that makes sense.]",
     images: { url: "https://nextcloud.ohheyandie.gay/index.php/apps/files_sharing/publicpreview/dibrXQTzDFeLzop?file=/stickers/8.png&fileId=674&x=1920&y=1080&a=true&etag=8df8cd5e36418b4b90428754c4b186c" }
-  },
-  viewport: "theme-color=#e28743, width=device-width, initial-scale=1",
+  }
 };
+
+export const viewport: Viewport = {
+  themeColor: "#e28743"
+}
 
 export default function RootLayout({
   children,
