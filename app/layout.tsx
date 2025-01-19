b6d06adc9e4937dc5746e8cf210eae97829d28bd
dumbspacedog <hello@enbyandie.moe>
2025-01-19 15:04:49 -0500

    add meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,15 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "NoChoccyWaffles",
   description: "The catch-all portfolio for my projects. [If, that makes sense.]",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://nochoccywaffles.com",
+    title: "NoChoccyWaffles",
+    description: "The catch-all portfolio for my projects. [If, that makes sense.]",
+    images: [{ url: "https://nextcloud.ohheyandie.gay/index.php/apps/files_sharing/publicpreview/dibrXQTzDFeLzop?file=/stickers/8.png&fileId=674&x=1920&y=1080&a=true&etag=8df8cd5e36418b4b90428754c4b186c" }]
+  },
+  themeColor: "#e28743"
 };
 
 export default function RootLayout({
